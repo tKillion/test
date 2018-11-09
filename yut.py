a8f71415fe7ae0c1b84fd751cc1e7d9edc9a7797
iamnotdavidhasselhoff <43529015+iamnotdavidhasselhoff@users.noreply.github.com>
2018-11-08 20:01:39 -0600

    Add files via upload

---
+++ yut.py
@@ -0,0 +1,14 @@
+print('This is an example of print function')
+
+print('we\'re going to the store')
+
+print('sup' + 'bitch')
+
+print('hi '+str(5))
+
+print(int('8')+5)
+
+print(float('8.5')+5)
+
+
+      
